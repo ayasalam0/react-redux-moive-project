fe2d4ec87f0fe04a6cb27f2bbab3487260096864
Zahraa <sahrayarub@yahoo.de>
2021-09-01 20:33:52 +0300

    feat(bannerSlider): add bannerSlider component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./styles/styles.css";
 import Navbar from "./components/header/Navbar";
+import BannerSlider from "./components/header/BannerSlider";
 import Home from "./components/Home";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <BannerSlider />
       </header>
       <Home />
     </div>
---
+++ src/components/header/BannerSlider.jsx
@@ -0,0 +1,5 @@
+function BannerSlider() {
+  return <div className="banner-slider"></div>;
+}
+
+export default BannerSlider;
