9d8f5dbb162dac265eaa4c584dcfd14991e15cfa
Zahraa <sahrayarub@yahoo.de>
2021-09-01 20:01:21 +0300

    fix(style): add styling add fix home style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./styles/styles.css";
 import Navbar from "./components/header/Navbar";
 import Home from "./components/Home";
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,14 +2,24 @@ function Home() {
   return (
     <div>
       <div className="trending-section">
-        <div className="row">
-          {/* Movie card component */}
-          {/* display as slider */}
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h2>Trending</h2>
+            </div>
+            {/* Movie card component */}
+            {/* display as slider */}
+          </div>
         </div>
       </div>
       <div className="popular-section">
-        <div className="row">
-          <div className="col-md-5">{/* Movie card component */}</div>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h2>Popular</h2>
+            </div>
+            <div className="col-md-5">{/* Movie card component */}</div>
+          </div>
         </div>
       </div>
     </div>
